2170508fd7a4334c11a856b9774b422c96502172
Björn Englesson <benglesson@gmail.com>
2020-02-21 13:05:15 +0100

    Add custom entry for future designing 2.

---
+++ TimerApp.iOS/Custom Renderers/CustomEntryRenderer.cs
@@ -0,0 +1,20 @@
+﻿using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using TimerApp;
+using TimerApp.iOS;
+
+[assembly: ExportRenderer(typeof(CustomEntry), typeof(CustomEntryRenderer))]
+namespace TimerApp.iOS {
+    public class CustomEntryRenderer : EntryRenderer {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e) {
+            base.OnElementChanged(e);
+
+            if(Control != null) {
+                Control.BorderStyle = UITextBorderStyle.None;
+                Control.Layer.CornerRadius = 10;
+                Control.TextColor = UIColor.White;
+            }
+        }
+    }
+}
---
+++ TimerApp/CustomRenderers/CustomEntry.cs
@@ -0,0 +1,6 @@
+﻿using Xamarin.Forms;
+
+namespace TimerApp {
+    public class CustomEntry : Entry {
+    }
+}
